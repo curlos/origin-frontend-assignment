c9cf6042fba2ee9e215a0d6cbc1d941eb1e3691e
Carlos Martinez <curlosmart@gmail.com>
2022-03-06 22:15:50 -0500

    Add structure for middle wrapper and card

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import Card from './components/Card';
 import Navbar from './components/Navbar';
 
 const Container = styled.div`
@@ -8,10 +9,38 @@ const Container = styled.div`
   max-width: 100%;
   background-color: #F4F8FA;
 `
+
+const MiddleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const InnerMiddleWrapper = styled.div`
+  width: 560px;
+  text-align: center;
+`
+
+const SavingGoalTitle = styled.div`
+  font-size: 20px;
+  line-height: 24px;
+
+  span {
+    font-weight: 600;
+  }
+`
+
 export function App(): JSX.Element {
   return (
     <Container>
       <Navbar />
+      <MiddleWrapper>
+        <InnerMiddleWrapper>
+          <SavingGoalTitle>Let's plan your <span>saving goal.</span></SavingGoalTitle>
+          <Card />
+        </InnerMiddleWrapper>
+      </MiddleWrapper>
     </Container>
   )
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: #FFFFFF;
+  border-radius: 8px;
+  box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
+  padding: 20px;
+  text-align: start;
+`
+
+export default function Card() {
+  return (
+    <Container>
+
+      Card
+    </Container>
+  )
+}
